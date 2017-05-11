4c0a2733f870faaf75009f7ba88867f19220292d
yoselalberto <yoselerratil@gmail.com>
2017-05-11 02:09:40 -0500

    add variable names, add subject of observation, and activity name

--- run_analysis.R
+++ run_analysis.R
@@ -16,25 +16,41 @@ train_raw <- read_table("UCI_HAR_Dataset/train/X_train.txt", col_names = FALSE)
 test_raw  <- read_table("UCI_HAR_Dataset/test/X_test.txt", col_names = FALSE)
 # labels
 labels_train <- read_csv("UCI_HAR_Dataset/train/y_train.txt", col_names = FALSE)
-labels_test <- read_csv("UCI_HAR_Dataset/test/y_test.txt", col_names = FALSE)
+labels_test  <- read_csv("UCI_HAR_Dataset/test/y_test.txt", col_names = FALSE)
 # subjets
 subject_train <- read_csv("UCI_HAR_Dataset/train/subject_train.txt",  col_names = FALSE)
 subject_test  <- read_csv("UCI_HAR_Dataset/test/subject_test.txt",  col_names = FALSE)
 # features
-features_raw  <- read_delim("UCI_HAR_Dataset/features.txt", col_names = FALSE, delim = " ")
+features_raw  <- read_table2("UCI_HAR_Dataset/features.txt", col_names = FALSE)
+# activity names
+activity <- read_table("UCI_HAR_Dataset/activity_labels.txt", col_names = FALSE)
 
 
 # preprocesing ------------------------------------------------------------
 
 features <- select(features_raw, -X1)
 
-# joining -----------------------------------------------------------------
+
+# add names ---------------------------------------------------------------
+
+names(train_raw) <- features$X2
+names(test_raw)  <- features$X2
 
 
+# add people --------------------------------------------------------------
+
+train <- subject_train %>% rename(subject = X1) %>% bind_cols(train_raw)
+test  <- subject_test  %>% rename(subject = X1) %>% bind_cols(test_raw)
 
 
+# add activity ------------------------------------------------------------
 
+activity_vector <- activity$X2
+names(activity_vector) <- activity$X1
 
+train_labeled <- labels_train %>% rename(activity = X1) %>% 
+    mutate(activity = str_replace_all(activity, pattern = activity_vector)) %>%
+    bind_cols(train)
 
 
 
