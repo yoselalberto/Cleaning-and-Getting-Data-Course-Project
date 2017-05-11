585535a39c9df0f87d1bd0d283cbbad40b0c7b0b
yoselalberto <yoselerratil@gmail.com>
2017-05-11 03:25:49 -0500

    improve documentation

--- run_analysis.R
+++ run_analysis.R
@@ -17,7 +17,7 @@ folder_name <- "UCI HAR Dataset"
 folder_name_zip <- "UCI HAR Dataset.zip" 
 # downloads data, if not present
 if (!file.exists(folder_name)) {
-    # download info
+    # download data
     url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     download.file(url, folder_name_zip)
 }
@@ -44,7 +44,8 @@ features_raw  <- read_table2("UCI HAR Dataset/features.txt", col_names = FALSE)
 activity <- read_table("UCI HAR Dataset/activity_labels.txt", col_names = FALSE)
 
 
-# preprocesing ------------------------------------------------------------
+
+# keep variable names -----------------------------------------------------
 
 features <- select(features_raw, -X1)
 
@@ -55,7 +56,7 @@ names(train_raw) <- features$X2
 names(test_raw)  <- features$X2
 
 
-# add people --------------------------------------------------------------
+# add subject ------------------------------------------------------------
 
 train <- subject_train %>% rename(subject = X1) %>% bind_cols(train_raw)
 test  <- subject_test  %>% rename(subject = X1) %>% bind_cols(test_raw)
@@ -66,6 +67,7 @@ test  <- subject_test  %>% rename(subject = X1) %>% bind_cols(test_raw)
 # vector to replace integers with labels
 activity_vector <- activity$X2
 names(activity_vector) <- activity$X1
+## Replace numbers for activity labels, and add them to data
 # train
 train_labeled <- labels_train %>% rename(activity = X1) %>% 
     mutate(activity = str_replace_all(activity, pattern = activity_vector)) %>%
@@ -78,6 +80,7 @@ test_labeled <- labels_test %>% rename(activity = X1) %>%
 
 # merging -----------------------------------------------------------------
 
+# join training and test data
 data_merged <- rbind(train_labeled, test_labeled)
 
 
@@ -85,6 +88,7 @@ data_merged <- rbind(train_labeled, test_labeled)
 
 # position of such variables
 position_mean_or_std <- str_detect(features$X2, pattern = "(mean)|(std)") %>% which() + 2
+# add two because I add to variables and the beginning
 data <- data_merged[, c(1, 2, position_mean_or_std)]
 # improve variable names
 names(data) <- str_replace_all(string = names(data), 
@@ -93,7 +97,9 @@ names(data) <- str_replace_all(string = names(data),
 
 #  average of each variable for each activity and each subject ------------
 
-data_averages <- data %>% gather(key = variable, value = value, -activity, -subject) %>% 
+# tidy data, a variable per column, the get respective averages
+data_averages <- data %>%
+    gather(key = variable, value = value, -activity, -subject) %>% 
     group_by(activity, subject, variable) %>% 
     summarise(average = mean(value, na.rm = TRUE))
 
